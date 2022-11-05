d1cc122871ca3b9a0a2a740163c564c879ce0768
Matt Todd <matt.c.todd@gmail.com>
2022-11-04 21:42:33 -0500

    removed unused lines

--- app.py
+++ app.py
@@ -2,14 +2,9 @@ import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
-my_sample = pd.read_csv('./test.csv')
-vars = ['Rentals', 'TotalCost']
-df = my_sample[vars]
-print(df)
-
-my_sample['year_f'] = pd.Categorical(my_sample.Year)
-my_model = smf.ols(formula = 'TotalCost ~ Rentals', data = my_sample).fit()
-print(my_model.summary())
+df = pd.read_csv('./test.csv')
+model = smf.ols(formula = 'TotalCost ~ Rentals', data = df).fit()
+print(model.summary())
 
 img = sm.graphics.plot_partregress('TotalCost', 'Rentals', ['Rentals'], data=df)
 img.savefig('test.png')
